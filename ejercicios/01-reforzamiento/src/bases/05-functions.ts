29b98003ae7546564b5e97d70ac97f8cdfe49cf4
Simon Astudillo <spastudilloq@gmail.com>
2025-09-14 19:16:48 -0300

    19.Funciones con múltiples retornos

--- ejercicios/01-reforzamiento/src/bases/05-functions.ts
+++ ejercicios/01-reforzamiento/src/bases/05-functions.ts
@@ -6,29 +6,41 @@ const message: string = greet('Gokú');
 
 console.log(message);
 
-const greet2 = (name: string): string => {
-   return `Hola ${name}`;
-}
+const greet2 = (name: string): string => `Hola ${name}`;
 
 
 const message2: string = greet2('Vegeta');
 console.log(message2);
 
-function getUser() {
+interface User {
+   uid: string;
+   username: string;
+}
+
+
+function getUser(): User {
    return {
       uid: 'ABC-123',
       username: 'El_Papi123'
    }
 }
 
-const getUser2 = () => {
-   return {
-      uid: 'ABC-123',
-      username: 'El_Papi123'
-   }
-}
+const getUser2 = (): User => ({
+   uid: 'ABC-123',
+   username: 'El_Papi123'
+});
 
 const user = getUser();
 console.log(user);
 const user2 = getUser2();
 console.log(user2);
+
+const myNumbers: number[] = [1, 2, 3, 4, 5, 6];
+
+myNumbers.forEach(function (value) {
+   console.log({value})
+})
+
+myNumbers.forEach( (value) => console.log({value}) );
+
+myNumbers.forEach(console.log);
