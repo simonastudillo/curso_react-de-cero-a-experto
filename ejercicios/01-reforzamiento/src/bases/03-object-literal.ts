f67c417520fb766b5617ab1c55a0fe1ab28e5807
Simon Astudillo <spastudilloq@gmail.com>
2025-09-14 18:46:04 -0300

    16.Interfaces de TypeScript

--- ejercicios/01-reforzamiento/src/bases/03-object-literal.ts
+++ ejercicios/01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,4 +1,16 @@
-const ironman = {
+interface Person {
+   firstName: string;
+   lastName: string;
+   age:number;
+   address: Address;
+}
+
+interface Address {
+   postalCode: string;
+   city: string;
+}
+
+const ironman: Person = {
    firstName: 'Tony',
    lastName: 'Stark',
    age: 45,
@@ -6,14 +18,17 @@ const ironman = {
       postalCode: 'ABC123',
       city: 'New York'
    }
-}
+};
 
-const spiderman = structuredClone(ironman);
+const spiderman: Person = {
+   firstName: 'Peter',
+   lastName: 'Parker',
+   age: 22,
+   address: {
+      postalCode: 'ABC123',
+      city: 'San José'
+   }
+};
 
-spiderman.firstName = 'Peter';
-spiderman.lastName = 'Parker';
-spiderman.age = 22;
-spiderman.address.city = 'San José';
-
-console.log(spiderman)
-console.log(ironman)
+console.log(spiderman);
+console.log(ironman);
