b180653e880fd7bbc1f8e0c95d5babc8cd9f54bd
Simon Astudillo <spastudilloq@gmail.com>
2025-09-16 17:58:20 -0300

    39.Impresión de variables

--- ejercicios/02-first-steps/src/MyAwesomeApp.tsx
+++ ejercicios/02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,28 @@
+
+
+const firstName = 'Fernando';
+const lastName = 'Herrera';
+
+const favoriteGames = ['Elden Ring', 'Smash', 'Metal Gear'];
+const isActive = true;
+
+const address = {
+   zipCode: 'ABC-123',
+   country: 'Canadá'
+}
+
 export const MyAwesomeApp = () => {
+
    return (
       <>
-         <h1>Fernando</h1>
-         <h3>Herrera</h3>
+         <h1> {firstName} </h1>
+         <h3> {lastName} </h3>
+
+         <p>{favoriteGames.join(', ')}</p>
+         <p>{2 + 2}</p>
+
+         <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+         <p>{JSON.stringify(address)}</p>
       </>
    );
 }
