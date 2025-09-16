f5a2da989269f79c8d2da7c54cf9e497037d05c9
Simon Astudillo <spastudilloq@gmail.com>
2025-09-16 20:22:18 -0300

    44.Eventos de los elementos

--- ejercicios/02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ ejercicios/02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 interface Props {
    itemName: string;
    quantity?: number;
 }
 
 export const ItemCounter = ({ itemName, quantity }: Props) => {
+
+   // const [counter, setCounter] = useState(quantity);
+
+   const handleClick = () => {
+      console.log(`Click en ${itemName}`);
+   }
+
    return (
       <section style={{
          display: 'flex',
@@ -14,7 +23,9 @@ export const ItemCounter = ({ itemName, quantity }: Props) => {
          <span style={{
             width: 150
          }}>{itemName}</span>
-         <button>+1</button>
+         <button
+            onClick={handleClick}
+         >+1</button>
          <span>{quantity ?? 1}</span>
          <button>-1</button>
       </section>
