74f12bf8ebb9b7db6c241ed2d1c6d5d3f766120c
Simon Astudillo <spastudilloq@gmail.com>
2025-09-22 22:57:01 -0300

    61.Componentes fictícios - Mock components

--- ejercicios/02-first-steps/src/FirstStepsApp.test.tsx
+++ ejercicios/02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,7 +1,11 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
+
+vi.mock("./shopping-cart/ItemCounter", () => ({
+   ItemCounter: () => <div data-testid="ItemCounter" />
+}));
 
 describe('FirstStepsApp', () => {
 
@@ -10,6 +14,16 @@ describe('FirstStepsApp', () => {
       const { container } = render(<FirstStepsApp />);
       // Assert
       expect(container).toMatchSnapshot();
+   });
+
+   test('Should render the correct number of ItemCounter components', () => {
+      // Arrange
+      const divsExpected = 3;
+      render(<FirstStepsApp />);
+      // Act
+      const itemCounters = screen.getAllByTestId('ItemCounter');
+      // Assert
+      expect(itemCounters.length).toBe(divsExpected);
    });
 
 });
