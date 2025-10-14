90100abbc53fe98bf88ddc55c11016c25c846000
Simon Astudillo <spastudilloq@gmail.com>
2025-10-13 23:13:27 -0300

    107.Pruebas sobre useGifs - caché

--- ejercicios/03-gifs-app/src/gifs/hooks/useGifs.test.tsx
+++ ejercicios/03-gifs-app/src/gifs/hooks/useGifs.test.tsx
@@ -2,18 +2,8 @@ import { act, renderHook } from "@testing-library/react";
 import { describe, expect, test, vi } from "vitest";
 import { useGifs } from "./useGifs";
 import type { Gif } from "../interfaces/gif.interface";
+import * as gifsActions from "../actions/get-gifs-by-query.action";
 
-// const handleSearchMock = vi.fn();
-// const handleTermClickMock = vi.fn();
-
-// vi.mock('./useGifs', () => ({
-//    useGifs: () => ({
-//       gifs: [],
-//       previousTerms: [],
-//       handleSearch: handleSearchMock,
-//       handleTermClick: handleTermClickMock
-//    })
-// }));
 
 describe('useGifs', () => {
 
@@ -49,6 +39,72 @@ describe('useGifs', () => {
          await result.current.handleTermClick('goku');
       });
       expect(result.current.gifs.length).toBe(10);
+   });
+
+   test('Should return a list of gifs from cache', async () => {
+      // Arrange
+      const { result } = renderHook(() => useGifs());
+      // Act
+      await act(async () => {
+         await result.current.handleSearch('goku');
+      });
+      vi.spyOn(gifsActions, 'GetGifsByQuery')
+         .mockRejectedValue(new Error('No está obteniend el valor desde el cache'));
+
+      await act(async () => {
+         await result.current.handleSearch('goku');
+      });
+      // Assert
+      expect(result.current.gifs.length).toBe(10);
+   });
+
+   test('Should return no more than 8 previous term', async () => {
+      // Arrange
+      const preveiousTermMaxExpected = 8;
+      const preveiousTermExpected = [
+         'goku9',
+         'goku8',
+         'goku7',
+         'goku6',
+         'goku5',
+         'goku4',
+         'goku3',
+         'goku2',
+      ];
+      const { result } = renderHook(() => useGifs());
+      // Act
+      vi.spyOn(gifsActions, 'GetGifsByQuery')
+         .mockResolvedValue([]);
+      await act(async () => {
+         await result.current.handleSearch('goku1');
+      });
+      await act(async () => {
+         await result.current.handleSearch('goku2');
+      });
+      await act(async () => {
+         await result.current.handleSearch('goku3');
+      });
+      await act(async () => {
+         await result.current.handleSearch('goku4');
+      });
+      await act(async () => {
+         await result.current.handleSearch('goku5');
+      });
+      await act(async () => {
+         await result.current.handleSearch('goku6');
+      });
+      await act(async () => {
+         await result.current.handleSearch('goku7');
+      });
+      await act(async () => {
+         await result.current.handleSearch('goku8');
+      });
+      await act(async () => {
+         await result.current.handleSearch('goku9');
+      });
+      // Assert
+      expect(result.current.previousTerms.length).toBe(preveiousTermMaxExpected);
+      expect(result.current.previousTerms).toStrictEqual(preveiousTermExpected);
    });
 
 });
