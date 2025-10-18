cc1577f2e4959853e1ffb22ea57fe7bb2e9c8c15
Simon Astudillo <spastudilloq@gmail.com>
2025-10-17 23:34:14 -0300

    124.Conectar varios custom Hooks entre sí

---
+++ ejercicios/04-hooks-app/src/03-examples/PokePage.tsx
@@ -0,0 +1,33 @@
+import { useCounter } from "../hooks/useCounter";
+
+export const PokemonPage = () => {
+
+   const { counter, increment, decrement } = useCounter(1);
+
+   return (
+      <div className="bg-gradient flex flex-col items-center">
+         <h1 className="text-2xl font-thin text-white">Pokémon</h1>
+         <h3 className="text-xl font-bold text-white">#{counter} Bulbasaur</h3>
+         <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${counter}.png`}
+            alt=""
+         />
+
+         <div className="flex gap-2">
+
+            <button
+               onClick={decrement}
+               className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">
+               Anterior
+            </button>
+
+            <button
+               onClick={increment}
+               className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">
+               Siguiente
+            </button>
+
+         </div>
+      </div>
+   );
+};
--- ejercicios/04-hooks-app/src/main.tsx
+++ ejercicios/04-hooks-app/src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 import './index.css';
-import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
+import { PokemonPage } from './03-examples/PokePage';
+// import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 // import { TrafficLight } from './01-useState/TrafficLight';
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       {/* <HooksApp /> */}
       {/* <TrafficLight /> */}
       {/* <TrafficLightWithEffect /> */}
-      <TrafficLightWithHook />
+      {/* <TrafficLightWithHook /> */}
+      <PokemonPage />
    </StrictMode>,
 )
