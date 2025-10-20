a6241dc10489f3c9c4f468b5bad152711f688c72
Simon Astudillo <spastudilloq@gmail.com>
2025-10-19 23:08:31 -0300

    135.Configuraci´pon de la función reducer

--- ejercicios/04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
+++ ejercicios/04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
@@ -13,10 +13,48 @@ interface TasksState {
 
 type TaskAction =
    { type: 'ADD_TODO', payload: string }
-   | { type: 'DELETE_TODO', payload: number }
    | { type: 'TOGGLE_TODO', payload: number }
+   | { type: 'DELETE_TODO', payload: number }
 
-export const tasksReducer = (state: TasksState, action: TaskAction): TasksState => {
+export const tasksReducer = (state: TasksState, { type, payload }: TaskAction): TasksState => {
 
-   return state;
+   switch (type) {
+      case 'ADD_TODO': {
+         const newTodo: Todo = {
+            id: Date.now(),
+            text: payload.trim(),
+            completed: false
+         };
+         return {
+            ...state,
+            todos: [newTodo, ...state.todos],
+            length: state.length + 1
+         };
+      }
+      case 'TOGGLE_TODO': {
+         const updatedTodos: Todo[] = state.todos.map((todo) => {
+            if (todo.id === payload) {
+               return { ...todo, completed: !todo.completed }
+            }
+            return todo;
+         });
+
+         return {
+            ...state,
+            todos: updatedTodos,
+            completed: updatedTodos.filter((todo) => todo.completed).length,
+            pending: updatedTodos.filter((todo) => !todo.completed).length
+         }
+      }
+      case 'DELETE_TODO': {
+
+         return {
+            ...state,
+            todos: state.todos.filter((todo) => todo.id !== payload),
+            length: state.length - 1
+         }
+      }
+      default:
+         return state;
+   }
 };
