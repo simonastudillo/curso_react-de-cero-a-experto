14c4c027bf1e399c44bcb71f514b07f16fc59a5a
Simon Astudillo <spastudilloq@gmail.com>
2025-10-19 22:26:52 -0300

    133.Lista de tareas con useState

--- ejercicios/04-hooks-app/src/05-useReducer/TaskApp.tsx
+++ ejercicios/04-hooks-app/src/05-useReducer/TaskApp.tsx
@@ -18,23 +18,39 @@ export const TasksApp = () => {
    const [inputValue, setInputValue] = useState('');
 
    const addTodo = () => {
-      console.log('Agregar tarea', inputValue);
+      if (inputValue.length === 0) return;
 
+      const newTodo: Todo = {
+         id: Date.now(),
+         text: inputValue.trim(),
+         completed: false
+      };
+
+      setTodos([newTodo, ...todos]);
+      setInputValue('');
    };
 
    const toggleTodo = (id: number) => {
-      console.log('Cambiar de true a false', id);
-
+      const updatedTodos: Todo[] = todos.map((todo) => {
+         if (todo.id === id) {
+            return { ...todo, completed: !todo.completed }
+         }
+         return todo;
+      });
+      setTodos(updatedTodos);
    };
 
    const deleteTodo = (id: number) => {
-      console.log('Eliminar tarea', id);
-
+      const updatedTodos: Todo[] = todos.filter(
+         (todo) => todo.id !== id
+      );
+      setTodos(updatedTodos);
    };
 
    const handleKeyPress = (e: React.KeyboardEvent) => {
-      console.log('Presiono enter');
-
+      if (e.key === 'Enter') {
+         addTodo();
+      }
    };
 
    const completedCount = todos.filter((todo) => todo.completed).length;
