5d118a713125bb58fb1824802497cb6a319f5079
Simon Astudillo <spastudilloq@gmail.com>
2025-10-25 23:11:18 -0300

    153.useOptimistic - Preparación del ejercicio

--- ejercicios/04-hooks-app/src/main.tsx
+++ ejercicios/04-hooks-app/src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 import './index.css';
-import { MemoCounter } from './06-memos/MemoCounter';
+import { InstagromApp } from './07-useOptimistic/InstagromApp';
+// import { MemoCounter } from './06-memos/MemoCounter';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { ScrambleWords } from './05-useReducer/ScrambleWords';
 // import { TasksApp } from './05-useReducer/TaskApp';
@@ -23,6 +24,7 @@ createRoot(document.getElementById('root')!).render(
       {/* <TasksApp /> */}
       {/* <ScrambleWords /> */}
       {/* <MemoHook /> */}
-      <MemoCounter />
+      {/* <MemoCounter /> */}
+      <InstagromApp />
    </StrictMode>,
 )
