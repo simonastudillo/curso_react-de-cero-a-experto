4110b0168caec6cc2a68cd1e4c3bea35ed6c49e0
Simon Astudillo <spastudilloq@gmail.com>
2025-10-26 22:16:17 -0300

    164.Navegar entre pantallas - Diseño de páginas

--- ejercicios/04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
+++ ejercicios/04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router"
+
 export const AboutPage = () => {
    return (
-      <div>
-         <h1>AboutPage</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+         <h1 className="text-4xl font-bold">Página sobre mí</h1>
+         <hr />
+         <div className="flex flex-col gap-2">
+            <Link
+               to="/profile"
+               className="hover:text-blue-500 underline text-2xl"
+            >Perfil</Link>
+            <Link
+               to="/login"
+               className="hover:text-blue-500 underline text-2xl">Iniciar sesión</Link>
+         </div>
       </div>
    )
 }
--- ejercicios/04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
+++ ejercicios/04-hooks-app/src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Link } from "react-router"
+
 export const LoginPage = () => {
    return (
-      <div>
-         <h1>LoginPage</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+         <h1 className="text-4xl font-bold">Iniciar sesión</h1>
+         <hr />
+         <form className="flex flex-col gap-2 my-10">
+            <Input
+               type="number"
+               placeholder="ID del usuario "
+            />
+
+            <Button
+               type="submit"
+            >
+               Log in
+            </Button>
+         </form>
+
+         <Link to="/about">
+            <Button variant="ghost">
+               Volver a la página principal
+            </Button>
+         </Link>
       </div>
    )
 }
