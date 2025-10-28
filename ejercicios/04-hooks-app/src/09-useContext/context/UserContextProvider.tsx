57489da4e3155e6bb9438a510e47cf2fb351d1be
Simon Astudillo <spastudilloq@gmail.com>
2025-10-27 22:54:40 -0300

    166.Contexto de usuario - información global

--- ejercicios/04-hooks-app/src/09-useContext/context/UserContextProvider.tsx
+++ ejercicios/04-hooks-app/src/09-useContext/context/UserContextProvider.tsx
@@ -1,12 +1,45 @@
-import { type PropsWithChildren } from "react"
+import { createContext, useState, type PropsWithChildren } from "react"
+import type { User } from "../data/user-mock.data";
 
 // interface UserContextProps {
 //    children: React.ReactNode
 // }
 
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+interface UserContextProps {
+   // state
+   authStatus: AuthStatus;
+   user: User | null;
+   // Methods
+   login: (userId: number) => boolean;
+   logout: () => void;
+}
+
+export const UserContext = createContext({} as UserContextProps);
+
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
 
-   // const [name, setName] = useState('Simón');
+   const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
+   const [user, setUser] = useState<User | null>(null);
 
-   return <>{children}</>
+   const handleLogin = (userId: number) => {
+      console.log(userId);
+      return true;
+   }
+
+   const handleLogOut = () => {
+      console.log('logout')
+   }
+
+   return <UserContext
+      value={{
+         authStatus: authStatus,
+         user: null,
+         login: handleLogin,
+         logout: handleLogOut
+      }}
+   >
+      {children}
+   </UserContext>
 }
