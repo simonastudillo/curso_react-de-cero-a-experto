0bb16d17c7e0144a936babd50a1c23b77723e9fe
Simon Astudillo <spastudilloq@gmail.com>
2025-11-22 23:51:06 -0300

    195.Mostrar la data de los héroes

--- ejercicios/05-heroes-app/src/heroes/components/HeroGrid.tsx
+++ ejercicios/05-heroes-app/src/heroes/components/HeroGrid.tsx
@@ -1,13 +1,18 @@
+import type { Hero } from "../types/hero.interface"
 import { HeroGridCard } from "./HeroGridCard"
 
-export const HeroGrid = () => {
+interface HeroGridProps {
+   heroes: Hero[];
+}
+
+export const HeroGrid = ({ heroes }: HeroGridProps) => {
    return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
-         {/* Hero Card 1 - Superman */}
-         <HeroGridCard />
-         <HeroGridCard />
-         <HeroGridCard />
-         <HeroGridCard />
+         {
+            heroes.map(hero => (
+               <HeroGridCard key={hero.id} hero={hero} />
+            ))
+         }
       </div>
    )
 }
--- ejercicios/05-heroes-app/src/heroes/pages/home/HomePage.tsx
+++ ejercicios/05-heroes-app/src/heroes/pages/home/HomePage.tsx
@@ -7,6 +7,7 @@ import { CustomPagination } from "@/components/custom/CustomPagination"
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs"
 import { getHeroesByPageAction } from "@/heroes/actions/get-heroes-by-page.action"
 import { useQuery } from "@tanstack/react-query"
+import { heroes } from '../../../../../01-reforzamiento/src/data/heroes.data';
 
 type tabType = "all" | "favorites" | "heroes" | "villains";
 
@@ -14,11 +15,13 @@ export const HomePage = () => {
 
    const [activeTab, setActiveTab] = useState<tabType>("all");
 
-   const { data, isLoading } = useQuery({
+   const { data: heroesResponse, isLoading } = useQuery({
       queryKey: ['heroes'],
       queryFn: () => getHeroesByPageAction(),
       staleTime: 1000 * 60 * 5, // 5 minutes
    })
+
+   const { heroes = [], total, pages = 1 } = heroesResponse || {};
 
    return (
       <>
@@ -58,25 +61,25 @@ export const HomePage = () => {
             <TabsContent value="all">
                <h1>Todos los personajes</h1>
                {/* Mostrar todos los personajes */}
-               <HeroGrid />
+               <HeroGrid heroes={heroes} />
             </TabsContent>
             <TabsContent value="favorites">
                <h1>Favoritos</h1>
-               <HeroGrid />
+               <HeroGrid heroes={heroes} />
             </TabsContent>
             <TabsContent value="heroes">
                <h1>Heroes</h1>
-               <HeroGrid />
+               <HeroGrid heroes={heroes} />
             </TabsContent>
             <TabsContent value="villains">
                <h1>Villains</h1>
-               <HeroGrid />
+               <HeroGrid heroes={heroes} />
             </TabsContent>
          </Tabs>
 
 
          {/* Pagination */}
-         <CustomPagination totalPages={8} />
+         <CustomPagination totalPages={pages} />
       </>
    )
 }
