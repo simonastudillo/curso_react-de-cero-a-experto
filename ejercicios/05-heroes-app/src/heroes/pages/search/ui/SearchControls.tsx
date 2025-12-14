a29876d400fca35cd403c49fc1f0726282e4e26e
Simon Astudillo <spastudilloq@gmail.com>
2025-12-13 22:18:43 -0300

    212.useRef - En valores de inputs

--- ejercicios/05-heroes-app/src/heroes/pages/search/ui/SearchControls.tsx
+++ ejercicios/05-heroes-app/src/heroes/pages/search/ui/SearchControls.tsx
@@ -1,15 +1,34 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Search, Filter, SortAsc, Grid, Plus } from "lucide-react"
+import { useRef } from "react"
+import { useSearchParams } from "react-router"
 
 export const SearchControls = () => {
+
+   const [searchParams, setSearchParams] = useSearchParams();
+   const inputRef = useRef<HTMLInputElement>(null);
+
+   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === 'Enter') {
+         setSearchParams((prev) => {
+            prev.set("name", inputRef.current?.value || "");
+            return prev;
+         });
+      }
+   }
+
    return (
       <>
          <div className="flex flex-col lg:flex-row gap-4 mb-8">
             {/* Search */}
             <div className="relative flex-1">
                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
-               <Input placeholder="Search heroes, villains, powers, teams..." className="pl-12 h-12 text-lg bg-white" />
+               <Input placeholder="Search heroes, villains, powers, teams..." className="pl-12 h-12 text-lg bg-white"
+                  ref={inputRef}
+                  onKeyDown={handleKeyDown}
+                  defaultValue={searchParams.get('name') ?? ''}
+               />
             </div>
 
             {/* Action buttons */}
