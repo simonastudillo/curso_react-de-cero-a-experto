ef77a04dc2501d6c08d291e4f20e030fb43d688c
Simon Astudillo <spastudilloq@gmail.com>
2026-04-13 22:49:46 -0400

    231. Parte 2 - Test - FavoriteHero Context

--- ejercicios/05-heroes-app/src/heroes/context/FavoriteHeroContext.test.tsx
+++ ejercicios/05-heroes-app/src/heroes/context/FavoriteHeroContext.test.tsx
@@ -1,7 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { FavoriteHeroContext, FavoriteHeroProvider } from "./FavoriteHeroContext";
 import { use } from "react";
+import type { Hero } from "../types/hero.interface";
+
+
+const mockHero = {
+   id: '1',
+   name: 'Batman'
+} as Hero;
 
 const TestComponent = () => {
 
@@ -24,6 +31,13 @@ const TestComponent = () => {
                ))
             }
          </div>
+         <button data-testid="toggle-favorite"
+            onClick={() => toggleFavorite(mockHero)}>
+            Toggle Favorite
+         </button>
+         <div data-testid="is-favorite">
+            {isFavorite(mockHero) ? 'Yes' : 'No'}
+         </div>
       </div>
    );
 };
@@ -38,10 +52,25 @@ describe('FavoriteHeroContext', () => {
       // Assert
       renderContextTest();
       // Act
-      screen.debug();
+      // screen.debug();
       // Arrange
       expect(screen.getByTestId('favorite-count').textContent).toBe('0');
       expect(screen.getByTestId('favorite-list').children.length).toBe(0);
+   });
+
+
+   test('Should add hero to favorites when toggleFavorite is called with new hero', async () => {
+      // Assert
+      renderContextTest();
+      const button = screen.getByTestId('toggle-favorite');
+      // Act
+      fireEvent.click(button);
+      screen.debug();
+      // Arrange
+      expect(screen.getByTestId('is-favorite').textContent).toBe('Yes');
+      expect(screen.getByTestId('favorite-hero').textContent).toBe('Batman');
+      expect(JSON.parse(localStorage.getItem('favoriteHeroes') || '[]')).toEqual([mockHero]);
+
    });
 
 });
