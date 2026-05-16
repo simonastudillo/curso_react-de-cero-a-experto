8e8251fda5cb0ab647119cc3be346ca0377d68a3
Simon Astudillo <spastudilloq@gmail.com>
2026-05-15 23:10:16 -0400

    240. Parte 2 - Test - CustomPagination

--- ejercicios/05-heroes-app/src/components/custom/CustomPagination.test.tsx
+++ ejercicios/05-heroes-app/src/components/custom/CustomPagination.test.tsx
@@ -4,9 +4,9 @@ import { CustomPagination } from "./CustomPagination";
 import { MemoryRouter } from "react-router";
 import type { PropsWithChildren } from "react";
 
-const renderWithRouter = (component: React.ReactElement) => {
+const renderWithRouter = (component: React.ReactElement, initialEntries?: string[]) => {
    return render(
-      <MemoryRouter>
+      <MemoryRouter initialEntries={initialEntries}>
          {component}
       </MemoryRouter>
    );
@@ -21,7 +21,7 @@ describe('CustomPagination', () => {
    test('Should render component with default values', () => {
       renderWithRouter(<CustomPagination totalPages={5} />);
 
-      screen.debug();
+      // screen.debug();
       expect(screen.getByText('Anterior')).toBeDefined();
       expect(screen.getByText('Siguiente')).toBeDefined();
       expect(screen.getByText('1')).toBeDefined();
@@ -29,5 +29,31 @@ describe('CustomPagination', () => {
       expect(screen.getByText('3')).toBeDefined();
       expect(screen.getByText('4')).toBeDefined();
       expect(screen.getByText('5')).toBeDefined();
+   });
+
+   test('Should disabled previous button on first page', () => {
+      renderWithRouter(<CustomPagination totalPages={5} />);
+
+      const previousButton = screen.getByText('Anterior');
+      expect(previousButton.getAttributeNames()).toContain('disabled');
+      // screen.debug(previousButton);
+   });
+
+   test('Should disabled next button on last page', () => {
+      renderWithRouter(<CustomPagination totalPages={5} />, ['/heroes?page=5']);
+
+      const nextButton = screen.getByText('Siguiente');
+      expect(nextButton.getAttributeNames()).toContain('disabled');
+      // screen.debug(nextButton);
+   });
+
+   test('Should disabled button 3 when current page is 3', () => {
+      renderWithRouter(<CustomPagination totalPages={5} />, ['/heroes?page=3']);
+
+      const button3 = screen.getByText('3');
+      const button2 = screen.getByText('2');
+      // screen.debug(button3);
+      expect(button2.getAttribute('variant')).toBe('outline');
+      expect(button3.getAttribute('variant')).toBe('default');
    });
 });
