57f364aaaf948a729d7aa8f8a928190fa49afda2
Simon Astudillo <spastudilloq@gmail.com>
2026-05-22 12:11:56 -0400

    270.Diseño - Panel administrativo

--- ejercicios/07-teslo-shop/teslo-shop/src/admin/layout/AdminLayout.tsx
+++ ejercicios/07-teslo-shop/teslo-shop/src/admin/layout/AdminLayout.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+import { AdminHeader } from "../components/AdminHeader";
+import { AdminSidebar } from "../components/AdminSidebar";
+import { Outlet } from "react-router";
+
 const AdminLayout = () => {
+   const [sidebarCollapsed, setSidebarCollapsed] = useState<boolean>(false);
+
    return (
-      <div>AdminLayout</div>
+      <div className="bg-gray-50 flex">
+         <AdminSidebar
+            isCollapsed={sidebarCollapsed}
+            onToggle={() => setSidebarCollapsed(!sidebarCollapsed)}
+         />
+
+         <div className="flex-1 flex flex-col">
+            <AdminHeader />
+
+            <main className="flex-1 p-6">
+               <Outlet />
+            </main>
+         </div>
+      </div>
    )
 }
 
