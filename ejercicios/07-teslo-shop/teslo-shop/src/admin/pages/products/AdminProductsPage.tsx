dc97b42e98c8a33d932a4c8cfd9258753459628f
Simon Astudillo <spastudilloq@gmail.com>
2026-05-22 22:49:28 -0400

    273.Diseño - ProductPage

--- ejercicios/07-teslo-shop/teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
+++ ejercicios/07-teslo-shop/teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
@@ -14,12 +14,14 @@ export const AdminProductsPage = () => {
                title="Products"
                subtitle="Aqui puedes ver y administar tus productos"
             />
-            <Link to={`/admin/products/new`}>
-               <Button>
-                  <PlusIcon />
-                  Nuevo producto
-               </Button>
-            </Link>
+            <div className="flex justify-end mb-10 gap-4">
+               <Link to={`/admin/products/new`}>
+                  <Button>
+                     <PlusIcon />
+                     Nuevo producto
+                  </Button>
+               </Link>
+            </div>
          </div>
 
          <Table className="bg-white p-10 shadow--xs border border-gray-200 rounded mb-10">
