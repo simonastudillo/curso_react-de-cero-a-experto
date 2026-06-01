4cb08566f72504dc53109a5f8d35eafaa94001d8
Simon Astudillo <spastudilloq@gmail.com>
2026-05-31 21:32:53 -0400

    282. Acción - getProductsAction

--- ejercicios/07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
+++ ejercicios/07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -2,8 +2,12 @@ import { CustomPagination } from "@/components/custom/CustomPagination"
 import { products } from "@/mocks/products.mock"
 import { CustromJumbotron } from "@/shop/components/CustromJumbotron"
 import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { useProducts } from "@/shop/hooks/useProducts"
 
 export const HomePage = () => {
+
+   const { data } = useProducts();
+
    return (
       <>
          <CustromJumbotron title="Todos los productos" />
