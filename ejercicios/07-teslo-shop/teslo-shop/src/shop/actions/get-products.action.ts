4269ec38cc6f363b681fb5d9db6af392f665c6e3
Simon Astudillo <spastudilloq@gmail.com>
2026-06-08 22:01:22 -0400

    284. Aplicar paginación

--- ejercicios/07-teslo-shop/teslo-shop/src/shop/actions/get-products.action.ts
+++ ejercicios/07-teslo-shop/teslo-shop/src/shop/actions/get-products.action.ts
@@ -1,8 +1,20 @@
 import { tesloApi } from "@/api/tesloApi";
 import type { ProductsResponse } from "@/interfaces/products.response";
 
-export const getProductsAction = async (): Promise<ProductsResponse> => {
-   const { data } = await tesloApi.get<ProductsResponse>('/products');
+interface Options {
+   limit?: number | string;
+   offset?: number | string;
+}
+
+export const getProductsAction = async (options: Options): Promise<ProductsResponse> => {
+   const { limit, offset } = options;
+
+   const { data } = await tesloApi.get<ProductsResponse>('/products', {
+      params: {
+         limit,
+         offset
+      }
+   });
 
    const productsWithImageUrls = data.products.map(product => ({
       ...product,
--- ejercicios/07-teslo-shop/teslo-shop/src/shop/hooks/useProducts.tsx
+++ ejercicios/07-teslo-shop/teslo-shop/src/shop/hooks/useProducts.tsx
@@ -1,11 +1,21 @@
 import { useQuery } from "@tanstack/react-query"
 import { getProductsAction } from "../actions/get-products.action"
+import { useSearchParams } from "react-router";
 
 export const useProducts = () => {
    // TODO: Implementar lógica para obtener los productos utilizando React Query
+   const [searchParams] = useSearchParams();
+
+   const limit = searchParams.get('limit') || 9;
+   const page = searchParams.get('page') || 1;
+   const offset = (Number(page) - 1) * Number(limit);
 
    return useQuery({
-      queryKey: ['products'],
-      queryFn: getProductsAction
+      queryKey: ['products', { limit, offset }],
+      queryFn: () => getProductsAction({
+         limit: isNaN(+limit) ? 9 : limit,
+         offset: isNaN(offset) ? 0 : offset
+      }),
+      staleTime: 1000 * 60 * 5,
    })
 }
