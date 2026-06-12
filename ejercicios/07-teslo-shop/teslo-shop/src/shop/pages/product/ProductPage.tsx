d071e2862629207522307d74ce48d1177de41721
Simon Astudillo <spastudilloq@gmail.com>
2026-06-11 22:36:18 -0400

    295. Gestor de estado - Zustand

--- ejercicios/07-teslo-shop/teslo-shop/src/shop/pages/product/ProductPage.tsx
+++ ejercicios/07-teslo-shop/teslo-shop/src/shop/pages/product/ProductPage.tsx
@@ -1,5 +1,15 @@
+import { useCounterStore } from "@/auth/store/auth.store";
+import { Button } from "@base-ui/react/button"
+
 export const ProductPage = () => {
+
+   const { count, inc, dec } = useCounterStore();
+
    return (
-      <div>ProductPage</div>
+      <>
+         <h1 className="text-3xl font-montserrat">Count: {count}</h1>
+         <Button onClick={inc}>+1</Button>
+         <Button onClick={dec}>-1</Button>
+      </>
    )
 }
